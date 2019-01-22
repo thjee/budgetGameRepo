0ee4daa9068f2d3b6bee66535256815936440bd8
thapaliaj405 <thapaliaj405@ALB205-12.strose.edu>
2019-01-22 15:42:27 -0500

    name of the user

---
+++ src/softengRepo/Hello.java
@@ -0,0 +1,14 @@
+package softengRepo;
+import java.util.Scanner; 
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.print("NAME: ");
+        Scanner sc = new Scanner(System.in); 
+        String name = sc.nextLine();
+        System.out.print(name);
+
+	}
+
+}
