4c4a279e028fc0fd132b952bdbed8fb10d949bfe
JessiaK <jessyk520@gmail.com>
2019-01-23 20:37:42 -0500

    Update Hello.java

--- src/softengRepo/Hello.java
+++ src/softengRepo/Hello.java
@@ -1,13 +1,17 @@
+/*
+This program prompts the user to enter their name. 
+The program ends by displaying the name of the user running the program.
+*/
 package softengRepo;
 import java.util.Scanner; 
 
 public class Hello {
 
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); 
-        String name = sc.nextLine();
-	System.out.print("NAME: ");
-        System.out.print(name);
+        Scanner sc = new Scanner(System.in); //declared and initilialized the scanner
+        String name = sc.nextLine(); //name = the user input
+	System.out.print("NAME: "); 
+        System.out.print(name);  //printing out the name of the user 
 
 	}
 
