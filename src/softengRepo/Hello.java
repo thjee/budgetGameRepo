1f4053e4ebe95d11a6867fa72e6b310ebee126ca
thjee <43361160+thjee@users.noreply.github.com>
2019-01-23 20:03:25 -0500

    Update Hello.java

--- src/softengRepo/Hello.java
+++ src/softengRepo/Hello.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.print("NAME: ");
         Scanner sc = new Scanner(System.in); 
         String name = sc.nextLine();
+	System.out.print("NAME: ");
         System.out.print(name);
 
 	}
